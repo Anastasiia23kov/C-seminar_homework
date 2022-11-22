4680d08d9e889a99daabb11c52d6e1b556a9f523
Anastasiia <penkova23anastasia@gmail.com>
2022-11-22 15:04:59 +0700

    Задача 2. Принимает 2 числа, показывет, какое число больше и какое число меньше.

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,6 +1,8 @@
-﻿//Программа на вход принимает 2 числа и выдает, 
+﻿//Задача 2. 
+//Программа на вход принимает 2 числа и выдает, 
 //какое число большее, а какое меньшее.
 
+Console.Clear();
 Console.Write("Введите 1-ое число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2-ое число: ");
@@ -10,9 +12,13 @@ if (n > m)
 {
     Console.Write("max = ");
     Console.WriteLine(n);
+    Console.Write("min = ");
+    Console.WriteLine(m);
 }
 else
 {
     Console.Write("max = ");
     Console.WriteLine(m);
+    Console.Write("min = ");
+    Console.WriteLine(n);
 }
