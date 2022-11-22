64d0bca77d2d309793dbdee200a9f6bc2ba5de78
Anastasiia <penkova23anastasia@gmail.com>
2022-11-22 14:45:50 +0700

    Создала программу для определения максимального числа. Задача 2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Программа на вход принимает 2 числа и выдает, 
+//какое число большее, а какое меньшее.
+
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (n > m)
+{
+    Console.Write("max = ");
+    Console.WriteLine(n);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(m);
+}
